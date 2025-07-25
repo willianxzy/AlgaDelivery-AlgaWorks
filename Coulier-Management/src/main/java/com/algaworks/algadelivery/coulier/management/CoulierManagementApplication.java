92cd9a2992f07e9c8d77ae384abb6cdb6e7dd8a8
Willian Kuhn Littig <willianlittig14@gmail.com>
2025-07-25 10:51:26 -0300

    first commit

---
+++ Coulier-Management/src/main/java/com/algaworks/algadelivery/coulier/management/CoulierManagementApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algadelivery.coulier.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoulierManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoulierManagementApplication.class, args);
+	}
+
+}
